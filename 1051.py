ada1f4238a010f1a7a5468cb07755664034f3dd6
eduardovictor-duarte <eduardo.victor@sempreceub.com>
2026-04-13 14:38:03 -0300

    01

--- 1051.py
+++ 1051.py
@@ -14,3 +14,12 @@ else:
         Renda = Renda -1000.00
         if Renda <= 1500.00:
             imposto += Renda * 0.18
+            print(f"R$ {imposto:.2f}")
+        
+        else:
+            imposto += 1500.00 * 0.18
+            Renda = Renda - 1500.00
+            if Renda > 0.00:
+                imposto += Renda * 0.28
+                print(f"R$ {imposto:.2f}")
+                    
